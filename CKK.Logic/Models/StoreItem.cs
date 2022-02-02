4a93cf8d60f0678f4b753a060ff8d5da45b565e1
Austin <austin.hutmacher0113@omail.otech.edu>
2022-02-02 09:34:32 -0700

    continued work oon pd2

--- CKK.Logic/Models/StoreItem.cs
+++ CKK.Logic/Models/StoreItem.cs
@@ -22,7 +22,7 @@ namespace CKK.Logic.Models
       public Product Product { get; set; }         //auto-implemented property for Product
       public int Quantity { get; set; }            //auto-implemented property for Quantity
 
-      public int GetQuantity()                     //test
+      public int GetQuantity()                     
       {
          return Quantity;
       }
