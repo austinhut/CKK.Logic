9bd8fef863d92c2af94ed6ca982b6ac4ce1849c9
Austin <austin.hutmacher0113@omail.otech.edu>
2022-02-24 11:50:56 -0700

    continued work on PD

--- CKK.Logic/Models/ShoppingCart.cs
+++ CKK.Logic/Models/ShoppingCart.cs
@@ -25,10 +25,15 @@ namespace CKK.Logic.Models
         
         public ShoppingCartItem AddProduct(Product prod, int quantity)
         {
+            
+            //checking for a valid quantity of product before continuing 
             if (quantity < 1)
             {
                 return null;
             }
+           
+            //checking for a valid product (hence != null) AND checks to make sure that the product and it's
+            //ID are equal this instance of prod and it's corresponding ID.
             if (_Product1 != null && _Product1.GetProduct().GetId() == prod.GetId())
             {
                 _Product1.SetQuantity(_Product1.GetQuantity() + quantity);
@@ -41,20 +46,98 @@ namespace CKK.Logic.Models
 
                 return _Product2;
             }
+           
             else if (_Product3 != null && _Product3.GetProduct().GetId()  == prod.GetId())
             {
+                _Product3.SetQuantity(_Product3.GetQuantity() + quantity);
+
+                return _Product3;
+            }
+            
+            //checking for instance where a new product is added
+            
+            //the condition of the if statement checks to see if the new product is being added to _Product1.
+            //if that is the case, the product would not equal what was previously in _Product1, hence it being 
+            //equal to null. if the product is equal to null, we then set the value of _Product1 to the instance 
+            //of Product (prod) that we are working with. 
+            if (_Product1.GetProduct() == null)
+            {
+                _Product1.SetProduct(prod);
+
+                return _Product1;
+            }
+            if (_Product2.GetProduct() == null)
+            {
+                _Product2.SetProduct(prod);
+                
+                return _Product2;
+            }
+            else if (_Product3.GetProduct() == null)
+            {
+                _Product3.SetProduct(prod);
+
                 return _Product3;
             }
             return null;
-
         }
 
+      
+      //Calling above method with new paramters (Product = prod, quantity = 1)
+      public ShoppingCartItem AddProduct(Product prod)
+      {
+         return AddProduct(prod, 1);
 
-        //
-        //public ShoppingCartItem AddProduct(Product prod)
-        //{
-                
-        //}
+      }
+
+      public ShoppingCartItem RemoveProduct(Product prod, int quantity)
+      {
+         if (_Product1 != null && _Product1.GetProduct().GetId() == prod.GetId())
+         {
+            
+            _Product1.SetQuantity(_Product1.GetQuantity() - quantity);
+            
+            if (_Product1.GetQuantity() < 1)
+            {
+               return null;
+            }
+            
+            return _Product1;
+         }
+         else if (_Product2 != null && _Product2.GetProduct().GetId() == prod.GetId())
+         {
+            _Product2.SetQuantity(_Product2.GetQuantity() - quantity);
+
+            if (_Product2.GetQuantity() < 1)
+            {
+               return null;
+            }
+
+            return _Product2;
+         }
+         else if (_Product3 != null && _Product3.GetProduct().GetId() == prod.GetId())
+         {
+            _Product3.SetQuantity(_Product3.GetQuantity() - quantity);
+
+            if (_Product3.GetQuantity() < 1)
+            {
+               return null;
+            }
+
+            return _Product3;
+         }
+         return null;
+      }
+
+      //public ShoppingCartItem GetProductById(int id)
+      //{
+
+      //}
+
+      //public ShoppingCartItem GetProduct(int productNum)
+      //{
+
+      //}
+
 
 
 
