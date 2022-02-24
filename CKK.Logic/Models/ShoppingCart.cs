4e6181e67c2797642c46d30236c73ce36dfde815
Austin <austin.hutmacher0113@omail.otech.edu>
2022-02-23 22:29:54 -0700

    pd4

--- CKK.Logic/Models/ShoppingCart.cs
+++ CKK.Logic/Models/ShoppingCart.cs
@@ -4,55 +4,60 @@ namespace CKK.Logic.Models
 {
    public class ShoppingCart
    {
-      private Customer Customer;
-      private ShoppingCartItem _Product1;
-      private ShoppingCartItem _Product2;
-      private ShoppingCartItem _Product3;
+        private Customer Customer;              //customer instance variable
+        private ShoppingCartItem _Product1;     //shopping cart product 1 instance variable
+        private ShoppingCartItem _Product2;     //shopping cart product 2 instance variable
+        private ShoppingCartItem _Product3;     //shopping cart product 3 instance variable
 
-      private int _CustomerId;
+        
+        //Shopping Cart constructor
+        public ShoppingCart(Customer cust)
+        {
+            Customer = cust;    
+        }
+
+        //calling method GetId from Customer class
+        public int GetCustomerId()
+        {
+            return Customer.GetId();
+        }
+        
+        
+        public ShoppingCartItem AddProduct(Product prod, int quantity)
+        {
+            if (quantity < 1)
+            {
+                return null;
+            }
+            if (_Product1 != null && _Product1.GetProduct().GetId() == prod.GetId())
+            {
+                _Product1.SetQuantity(_Product1.GetQuantity() + quantity);
+
+                return _Product1;
+            }
+            else if (_Product2 != null && _Product2.GetProduct().GetId() == prod.GetId())
+            {
+                _Product2.SetQuantity(_Product2.GetQuantity() + quantity);
+
+                return _Product2;
+            }
+            else if (_Product3 != null && _Product3.GetProduct().GetId()  == prod.GetId())
+            {
+                return _Product3;
+            }
+            return null;
+
+        }
 
 
-      //Shopping Cart constructor
-      public ShoppingCart(Customer cust)
-      {
-         Customer = cust;
-      }
+        //
+        //public ShoppingCartItem AddProduct(Product prod)
+        //{
+                
+        //}
 
-      public int GetCustomerId()
-      {
-         return _CustomerId;
-      }
 
-      public ShoppingCartItem AddProduct(Product prod, int quantity)
-      {
-         
-      }
 
-      public void AddProduct(Product prod)
-      {
-         if (prod == null)
-         {
-            _Product1 = prod;
-         }
-         else if (prod == null)
-         {
-            _Product2 = prod;
-         }
-         else if (prod == null)
-         {
-            _Product3 = prod;
-         }
-      }
-
-      public ShoppingCartItem RemoveProduct(Product prod, int quantity)
-      {
-         
-      }
-
-      public decimal GetTotal()
-      {
-         
-      }
 
 
 
