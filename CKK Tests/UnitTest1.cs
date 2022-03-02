434d0e624c8619bc9eb4c7b31562302e1b1c3b08
Austin <austin.hutmacher0113@omail.otech.edu>
2022-03-02 11:52:34 -0700

    PD

--- CKK Tests/UnitTest1.cs
+++ CKK Tests/UnitTest1.cs
@@ -6,22 +6,72 @@ namespace CKK_Tests
 {
     public class UnitTest1
     {
+        private ShoppingCartItem _Product1;     //shopping cart product 1 instance variable
+        private ShoppingCartItem _Product2;     //shopping cart product 2 instance variable
+        private ShoppingCartItem _Product3;     //shopping cart product 3 instance variable
+
         [Fact]
         public void TotalOfProductIsCorrect()
         {
             //Arrange (Setting up test)
-            decimal price = 10.49m;
-            int quantity = 4;
+           
+            decimal expPrice = 10.49m;
+            decimal expQuantity = 3m;
 
-            decimal expected = 41.96m;
 
-            Product prod = new Product()
-            
+            //Customer cust = new Customer();
+            Product prod = new Product();
+            ShoppingCartItem cartItem = new ShoppingCartItem(prod, 3);
+            //ShoppingCart cart = new ShoppingCart(cust);
+
             //Act (Action that is being tested)
+           
+            decimal expected = expPrice * expQuantity;
+
+            decimal actual = cartItem.GetTotal();
+
+            //Assert (What is to be expected from test compared to actual value)
+           
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SucessfullAddProduct()
+        {
+            //Arrange (Setting up test)
             
+            Product expProd = new Product();
+            ShoppingCartItem expCartItem = new ShoppingCartItem (expProd, 2);
+
+            //Act (Action that is being tested)
+            var expected = expProd.GetId();
+
+
+            var actual = _Product1.GetProduct().GetId();
+
 
             //Assert (What is to be expected from test compared to actual value)
             Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SuccessfulRemoveProduct()
+        {
+            //Arrange (Setting up test)
+
+            Product expProd = new Product();
+            ShoppingCartItem expCartItem = new ShoppingCartItem(expProd, 1);
+
+            //Act (Action that is being tested)
+
+            
+
+
+
+
+
+            //Assert (What is to be expected from test compared to actual value)
+            //Assert.Equal(expProd, _Product1);
         }
     }
 }
