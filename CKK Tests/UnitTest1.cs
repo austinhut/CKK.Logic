5dbec4f5c4fd356710ada42620deaa5bd790479c
Austin <austin.hutmacher0113@omail.otech.edu>
2022-03-04 09:49:54 -0700

    pd

--- CKK Tests/UnitTest1.cs
+++ CKK Tests/UnitTest1.cs
@@ -6,9 +6,6 @@ namespace CKK_Tests
 {
     public class UnitTest1
     {
-        private ShoppingCartItem _Product1;     //shopping cart product 1 instance variable
-        private ShoppingCartItem _Product2;     //shopping cart product 2 instance variable
-        private ShoppingCartItem _Product3;     //shopping cart product 3 instance variable
 
         [Fact]
         public void TotalOfProductIsCorrect()
@@ -20,14 +17,12 @@ namespace CKK_Tests
                 decimal expPrice = 10.49m;
                 decimal expQuantity = 3m;
 
-
-                //Customer cust = new Customer();
                 Product prod = new Product();
                 ShoppingCartItem cartItem = new ShoppingCartItem(prod, 3);
-                //ShoppingCart cart = new ShoppingCart(cust);
+                
 
                 //Act (Action that is being tested)
-
+                
                 decimal expected = expPrice * expQuantity;
 
                 decimal actual = cartItem.GetTotal();
